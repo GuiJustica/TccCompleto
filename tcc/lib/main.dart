f4178736b90f740da2dd0183caa1138adb8ef516
GuiJustica <justicaguilherme@gmail.com>
2025-07-07 02:06:08 -0300

    so deixando padrao a primeira tela, nada de mais

--- tcc/lib/main.dart
+++ tcc/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Tutorial(),
+      home: SplashScreen(),
       routes: {
         'home': (context) => MyHomePage(),
         'cadorlogin': (context) => CadOrLogin(),
